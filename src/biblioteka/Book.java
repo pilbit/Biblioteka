9bc8f6237410a3ad2f6112d6007d1afb0eaaddea
Paweł Piluś <ppilus@localhost.localdomain>
2017-01-24 18:10:46 +0100

    odczyt,zapis,dodanie,wypozyczenie - dziala

--- src/biblioteka/Book.java
+++ src/biblioteka/Book.java
@@ -20,7 +20,7 @@ import java.io.LineNumberReader;
 public class Book extends Biblioteka
 {
     
-    private static int id;
+    private int id;
     private String tytul;
     private String autor;
     private String opis;
@@ -28,17 +28,60 @@ public class Book extends Biblioteka
     
     public Book()
     {
-        this.id ++ ;
+        //this.id ++ ;
     }
     
-    public void add()
+    public void add(int index)
     {
-        id++;
+        this.id = index;
     }
     
-    public void getData(int i)
+    public String getId(int n){
+        if((n+1)<=this.id)
+        {
+        return Integer.toString(n);
+        }else {
+            return Integer.toString(n);
+        }
+            
+    }
+    public void setTytul(String tytul){
+        this.tytul=tytul;
+    }
+    
+    public void setAutor(String autor){
+        this.autor=autor;
+    }
+    
+    public void setOpis(String opis){
+        this.opis=opis;
+    }
+    
+    public void setUserId(String userid){
+        this.user_id=Integer.valueOf(userid);
+    }
+    
+    public String getTytul(){
+        return tytul;
+    }
+    
+    
+    public String getAutor(){
+        return autor;
+    }
+    
+    public String getOpis(){
+        return opis;
+    }
+    
+    public String getUserId(){
+        return Integer.toString(user_id);
+    }
+    
+    public void getData(int obj_users, int i)
     {
-        if(i==0)
+        //for(int i=0; i<obj_users; i++){
+        if(i==0){
         System.out.println("* Zawartość Bazy Bibliotecznej * - Ksiązki - *");
         System.out.println("");
         System.out.println("----------------------------------------------");
@@ -53,48 +96,38 @@ public class Book extends Biblioteka
         System.out.println("ID użytkownika ksiązki: "+this.user_id);
         System.out.println("----------------------------------------------");
         System.out.println("");
-        
-    }
-    
-    public void setData(String[] tab,int ile)
-    {
-        if (ile == 0) {//sprawdzenie czy pierwsza ksiazka i zapis
-            tab[ile] = Integer.toString(id);
-            ile++;
-            this.tytul = tab[ile];
-            ile++;
-            this.autor = tab[ile];
-            ile++;
-            this.opis = tab[ile];
-            ile++;
-            this.user_id = Integer.valueOf(tab[ile]);
-        } else {//jesli nie piwerwsza ustawienie indexu tablicy
-            ile*=5;
-            tab[ile] = Integer.toString(id);
-            ile++;
-            this.tytul = tab[ile];
-            ile++;
-            this.autor = tab[ile];
-            ile++;
-            this.opis = tab[ile];
-            ile++;
-            this.user_id = Integer.valueOf(tab[ile]);
-
+        }else{
+            
+        System.out.println("----------------------------------------------");
+        System.out.println("Id    ksiązki         : "+this.id);
+        System.out.println("----------------------------------------------");
+        System.out.println("Tytuł ksiązki         : "+this.tytul);
+        System.out.println("--------------------------------------------------");
+        System.out.println("Autor ksiązki         : "+this.autor);
+        System.out.println("--------------------------------------------------");
+        System.out.println("Opis ksiązki          : "+this.opis);
+        System.out.println("--------------------------------------------------");
+        System.out.println("ID użytkownika ksiązki: "+this.user_id);
+        System.out.println("----------------------------------------------");
+        System.out.println("");
         }
+        //}
         
     }
     
-    
-    
-     
-    
-     public void write(String name) throws IOException
+    public void setData(String[] tab, int book_counter)//ile to index ksiazki moze buc 0(1)
     {
-        FileWriter fw = new FileWriter(name);
-        BufferedWriter bw = new BufferedWriter(fw);
-        
-        
-    }
+
+            this.add(book_counter);
+            int i = 1;
+            this.tytul = tab[i];
+            i++;
+            this.autor = tab[i];
+            i++;
+            this.opis = tab[i];
+            i++;
+            this.user_id = Integer.valueOf(tab[i]);
     
+    }
     
 }
